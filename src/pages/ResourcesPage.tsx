7e8c8cecd15d375acfa9ebd99691eb4e0bf7fa97
VitorYerme <meyervictorgz@gmail.com>
2026-01-30 12:47:59 +0100

    Update ResourcesPage.tsx

--- src/pages/ResourcesPage.tsx
+++ src/pages/ResourcesPage.tsx
@@ -47,7 +47,7 @@ export function ResourcesPage() {
     if (level === 'public') return true;
     if (!user) return false;
     if (level === 'members') return true;
-    if (level === 'marina') return profile?.role === 'marina_verified' || profile?.role === 'admin';
+    if (level === 'marina') return profile?.role === 'marina' && profile?.status === 'verified'
     return false;
   };
 

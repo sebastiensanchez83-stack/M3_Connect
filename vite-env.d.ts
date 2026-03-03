5de4213b50fbca63ae0aa8da478b782144a01e0c
sebastiensanchez83-stack <sebastien.sanchez83@gmail.com>
2026-03-03 18:06:45 +0100

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly  VITE_SUPABASE_URL: string
-  readonly VITE_SUPABASE_ANON_KEY: string
+  readonly  VITE_SUPABASE_URL: 'https://djjbgzasuomhyfvtlidi.supabase.co'
+  readonly VITE_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqamJnemFzdW9taHlmdnRsaWRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2MTQzODUsImV4cCI6MjA4NTE5MDM4NX0.7HiGbBFq-yR37z7F3gvqeZ71emvHKR-dR05HrAORbWA'
 }
 
 interface ImportMeta {
